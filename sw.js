cb7f7e27fcf97a537e53f2f009da061b0946123b
Daniel Dietrich <danieldietrich31@gmail.com>
2026-07-14 12:18:05 -0300

    modified:   styles.css 	modified:   sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Habilita funcionamento 100% offline e cache de recursos estáticos.
  */
 
-const CACHE_NAME = 'pinheiro-afiacoes-cache-v33';
+const CACHE_NAME = 'pinheiro-afiacoes-cache-v34';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
