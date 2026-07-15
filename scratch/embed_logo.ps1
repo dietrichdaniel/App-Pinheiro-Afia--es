172c3d286ec1d75463b1519cfb519afd2d696138
Daniel Dietrich <danieldietrich31@gmail.com>
2026-07-15 12:28:29 -0300

    modified:   icons/icon-maskable.svg 	modified:   icons/icon.svg 	modified:   scratch/embed_logo.ps1 	modified:   styles.css 	modified:   sw.js

--- scratch/embed_logo.ps1
+++ scratch/embed_logo.ps1
@@ -6,8 +6,12 @@ $svgMaskPath = Join-Path $parentDir "icons\icon-maskable.svg"
 $bytes = [IO.File]::ReadAllBytes($pngPath)
 $base64 = [Convert]::ToBase64String($bytes)
 
-$svgContent = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1000 1000' width='100%' height='100%'><image href='data:image/png;base64,$base64' x='0' y='0' width='1000' height='1000'/></svg>"
+# Template para favicon/UI (com recorte circular para remover cantos quadrados)
+$svgContent = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1000 1000' width='100%' height='100%'><defs><clipPath id='circleClip'><circle cx='500' cy='500' r='500'/></clipPath></defs><image href='data:image/png;base64,$base64' x='0' y='0' width='1000' height='1000' clip-path='url(#circleClip)'/></svg>"
+
+# Template para PWA maskable (com fundo escuro preenchido e imagem reduzida a 80% para ficar na Safe Zone e não cortar as bordas)
+$svgMaskContent = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1000 1000' width='100%' height='100%'><rect width='1000' height='1000' fill='#06080d'/><image href='data:image/png;base64,$base64' x='100' y='100' width='800' height='800'/></svg>"
 
 [IO.File]::WriteAllText($svgPath, $svgContent)
-[IO.File]::WriteAllText($svgMaskPath, $svgContent)
-Write-Output "Successfully embedded PNG inside SVG files!"
+[IO.File]::WriteAllText($svgMaskPath, $svgMaskContent)
+Write-Output "Successfully updated embedded PNG inside SVG files with masking and safe zone scaling!"
